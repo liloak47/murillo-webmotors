21de9fb2e05df4998200bafc533fcfca51739da4
Murillo  Augusto <murilo@nvgo.com.br>
2021-04-06 19:46:20 -0300

    fix bug autoPlay don't work

--- src/helpers/SettingsSlide/settings.js
+++ src/helpers/SettingsSlide/settings.js
@@ -1,9 +1,11 @@
 const settings = {
   dots: true,
   infinite: true,
-  speed: 500,
   slidesToShow: 1,
   slidesToScroll: 1,
+  autoplay: true,
+  speed: 800,
+  pauseOnHover: false,
 };
 
 export default settings;
